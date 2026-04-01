c95536d2127e28053a630df2bf06ee5b54bdf817
Gabriel Rumen <gabrielrumen@MacBook-Air-de-Gabriel.local>
2026-04-01 17:29:34 +0200

    disable zoom on mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <head>
         <meta name="theme-color" content="#3BBCD0" />
         <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body className="bg-black text-gray-50 min-h-screen">
         <Navigation />
